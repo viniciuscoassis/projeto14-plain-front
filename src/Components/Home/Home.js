e8db62bf52905c904899d45ba928e7c1b06e2e80
JouJou <joujou@pop-os.localdomain>
2022-09-14 19:21:42 -0300

    Adding sidebar menu and start screen

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,46 @@
+import styled from "styled-components";
+import Headers from "../Headers/Headers.js";
+import Homeimg from "../../assets/img/homeimg.svg";
+import Footer from "../Footer/Footer.js";
+import { useState } from "react";
+
+export default function Home() {
+  const [sidebar, setSideBar] = useState("close");
+  return (
+    <>
+      <Headers cartQuantity="1" />
+      <Container>
+        <div className="warnings">
+          <p>parcelamento de até 10x sem juros</p>
+          <div className="divisor"></div>
+          <p>5% de desconto para pagamentos no pix</p>
+        </div>
+        <img src={Homeimg} alt="product" />
+        <img src={Homeimg} alt="product" />
+      </Container>
+      <Footer />
+    </>
+  );
+}
+
+const Container = styled.main`
+  .warnings {
+    height: 73px;
+    text-align: center;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 8px;
+    background-color: black;
+    color: white;
+  }
+  .divisor {
+    height: 1px;
+    width: 162px;
+    background-color: white;
+  }
+  img {
+    margin-top: 11px;
+  }
+`;
